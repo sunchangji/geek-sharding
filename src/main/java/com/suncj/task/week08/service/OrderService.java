dff9563e62d544c0e043fb46c3b61c9b5562a12d
孙长纪 <sunchangji@sunchangjideMacBook-Pro.local>
2021-08-13 21:22:54 +0800

    基于ShardingSphere的Atomikos XA实现一个简单的分布式事务

--- src/main/java/com/suncj/task/week08/service/OrderService.java
+++ src/main/java/com/suncj/task/week08/service/OrderService.java
@@ -2,8 +2,11 @@ package com.suncj.task.week08.service;
 
 import com.suncj.task.week08.entity.OrderEntity;
 import com.suncj.task.week08.mapper.OrderMapper;
+import org.apache.shardingsphere.transaction.annotation.ShardingTransactionType;
+import org.apache.shardingsphere.transaction.core.TransactionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,15 +19,20 @@ public class OrderService {
     public List<OrderEntity> findList(Long userId,Long orderId) {
         return orderMapper.findList(userId,orderId);
     }
-
+    @Transactional
+    @ShardingTransactionType(TransactionType.XA)
     public void create(OrderEntity entity){
         orderMapper.create(entity);
     }
 
+    @Transactional
+    @ShardingTransactionType(TransactionType.XA)
     public int delete(Long userId,Long orderId){
         return orderMapper.delete(userId,orderId);
     }
 
+    @Transactional
+    @ShardingTransactionType(TransactionType.XA)
     public int update(Long userId,Long orderId){
         return orderMapper.update(userId+"ps",orderId,userId);
     }
